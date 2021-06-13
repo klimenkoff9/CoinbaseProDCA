26ed8f137af43268937b18b099766f9b8f355a12
klimenkoff9 <dennis.klimenkov9@gmail.com>
2021-06-12 22:50:36 -0400

    Set up recurring buy

--- source/controllers/buy.ts
+++ source/controllers/buy.ts
@@ -1,7 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
 import { CoinbasePro, MarketOrder, OrderSide, OrderType } from 'coinbase-pro-node';
+var cron = require('node-cron');
 
-const buy = async (_req: Request, res: Response, next: NextFunction) => {
+const buy = (_req: Request, res: Response, next: NextFunction) => {
     try {
         const auth = {
             apiKey: `${process.env.KEY}`,
@@ -10,25 +11,35 @@ const buy = async (_req: Request, res: Response, next: NextFunction) => {
             useSandbox: true
         };
         const client = new CoinbasePro(auth);
-       
+
         const order: MarketOrder = {
             type: OrderType.MARKET,
             product_id: 'BTC-USD',
             side: OrderSide.BUY,
-            funds: '25'
+            funds: '50'
         };
-
-        await client.rest.order.placeOrder(order);
+        console.log('check');
+        cron.schedule(
+            '*/1 * * * *',
+             () => {
+                client.rest.order.placeOrder(order);
+                console.log('bought');
+            },
+            {
+                scheduled: true,
+                timezone: 'America/New_York'
+            }
+        );
 
         return res.status(200).json({
             message: 'Success'
         });
-        
-        } catch (error) {
-            next(error);
-        };
+    } catch (error) {
+        next(error);
+    }
 };
 
 export default {
-    buy
+    buy,
+    stop
 };
